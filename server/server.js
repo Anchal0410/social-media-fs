d552090f53f8b18d8f8a7b00467c63f1a483101d
Anchal0410 <jainanchal0410@gmail.com>
2025-01-19 00:45:54 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -112,11 +112,11 @@ const authenticateToken = (req, res, next) => {
 
 
 // Testing
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'Server is working!' });
+app.get('/', (req, res) => {
+    res.json({ message: 'Server is running' });
   });
 
-
+  
 // Create initial admin user route 
 app.post('/api/setup-admin', async (req, res) => {
     try {
@@ -272,6 +272,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-
-module.exports = app;

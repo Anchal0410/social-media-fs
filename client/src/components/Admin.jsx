bcb657fd261f40d93fe42bd665d3aed14b2ef5a9
Anchal0410 <jainanchal0410@gmail.com>
2025-01-26 01:02:17 +0530

    link update

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -31,13 +31,16 @@ const AdminLogin = ({ onLogin }) => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/admin/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-      });
+      const response = await fetch(
+        "https://social-media-fs.onrender.com/api/admin/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
+        }
+      );
 
       const data = await response.json();
 
